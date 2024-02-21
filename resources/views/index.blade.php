3d0f55cc2ee464524106a2ac75f618fa5c9f4487
Uditha Mahindarathna <melan.udi@gmail.com>
2024-02-22 01:40:21 +0530

    blade template basics

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+Hello my name is {{ $name }} 
+
+@isset($age)
+   and Age is {{ $age }}
+@endisset
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return "Main Page";
+    return view('index', [
+        'name' => 'Uditha',
+        // 'age' => '28'
+    ]);
 });
 
 Route::get("/hello", function () {
