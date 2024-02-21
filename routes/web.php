708fe3a39cb75944053beb2870210e2a13e8df6a
Uditha Mahindarathna <melan.udi@gmail.com>
2024-02-22 01:26:14 +0530

    routing basics

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Main Page";
+});
+
+Route::get("/hello", function () {
+    return "Hello";
+})->name('hello'); //named routes
+
+Route::get("/hallo", function () {
+    return redirect()->route('hello');
+});
+
+Route::get("/greet/{name}", function ($name) {
+    return "Hello {$name}";
+});
+
+Route::fallback(function () {
+    return "404!";
 });
