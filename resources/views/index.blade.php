fffc0e32af4a9359cfd706587b1ec3b707511934
Uditha Mahindarathna <melan.udi@gmail.com>
2024-02-23 21:17:22 +0530

    blade directive changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,25 @@
-Hello my name is {{ $name }} 
+<h1>
+   List of Tasks
+</h1>
 
-@isset($age)
-   and Age is {{ $age }}
-@endisset
+{{-- <div>
+   @if (count($tasks))
+      @foreach ($tasks as $task)
+         <div>{{ $task->title }}</div>
+      @endforeach
+   @else
+      <div>There are no tasks</div>
+   @endif
+</div> --}}
+
+<hr>
+
+<div>
+   @forelse ($tasks as $task)
+      <div>
+         <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+      </div>
+   @empty
+      <div>There are no tasks</div>
+   @endforelse
+</div>
